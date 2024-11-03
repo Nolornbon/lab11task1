39e5fe3ce2ee05340620a1513a6e8afea355a773
Misha Smishnyi <mishasmish@gmail.com>
2024-11-03 20:15:45 +0200

    Добавьте файлы проекта.

---
+++ lab11task1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a0 = 1; 
+    int a1 = 3;  
+    int n = 10;  
+    int current;
+    
+    cout << "a0 = " << a0 << endl;
+    cout << "a1 = " << a1 << endl;
+   
+    for (int i = 2; i < n; i++) {
+        current = 7 * a1 - 3 * a0;  
+        cout << "a" << i << " = " << current << endl;
+        a0 = a1;
+        a1 = current;
+    }
+
+    return 0;
+}
